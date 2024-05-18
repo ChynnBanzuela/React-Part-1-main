d1561cd831f77f8f1106800a59c7e7ce7b43d50e
Chynn_Banzuela <chynnbanzuela49@gmail.com>
2024-05-18 23:12:31 +0800

    first commit

---
+++ README.md
@@ -0,0 +1,4 @@
+Members:
+Chynn Banzuela 
+Dianne Amondina
+Erica Binalla
---
+++ src/assets/Components/CoreConcepts.jsx
@@ -0,0 +1,16 @@
+const CoreConcept = ({image, title, description}) =>{
+    return (
+        <>
+            <li>
+                <img src={image} alt=""/>
+                <h1>{title}</h1>
+                <p>{description}</p>
+            </li>
+                
+        
+            
+        </>
+    )
+}
+
+export default CoreConcept;
